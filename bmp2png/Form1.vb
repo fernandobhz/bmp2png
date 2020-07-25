c2c87a8e542da999d7158d55e2e8f0b7b26e54e1
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 18:57:15 -0300

    old project migrated

---
+++ bmp2png/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO.Path
+
+Public Class Form1
+    Public Function ConvertBMP(ByVal BMPFullPath As String, _
+    ByVal imgFormat As ImageFormat) As Boolean
+
+        Dim bAns As Boolean
+        Dim sNewFile As String
+
+        Try
+            'bitmap class in system.drawing.imaging
+            Dim objBmp As New Bitmap(BMPFullPath)
+
+            'below 2 functions in system.io.path
+            sNewFile = GetDirectoryName(BMPFullPath) & "\"
+            sNewFile &= GetFileNameWithoutExtension(BMPFullPath)
+
+            sNewFile &= "." & imgFormat.ToString
+            objBmp.Save(sNewFile, imgFormat)
+
+            bAns = True 'return true on success
+        Catch
+            bAns = False 'return false on error
+        End Try
+        Return bAns
+
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        ConvertBMP("f:\desktop\ss.bmp", ImageFormat.Png)
+    End Sub
+End Class
